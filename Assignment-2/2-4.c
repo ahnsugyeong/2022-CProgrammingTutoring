453e0ced1f37e4c280f172543a0e6d9669729a23
suddiyo <suddii01@sju.ac.kr>
2022-03-31 17:23:09 +0900

    assignment-2

--- Assignment-2/2-4.c
+++ Assignment-2/2-4.c
@@ -48,79 +48,76 @@ int main() {
             }
         }
     } else {
-        if(num == 20) printf("twelve\n");
-        else {
-            switch (num / 10) {
-                case 2: {
-                    printf("twenty");
-                    break;
-                }
-                case 3: {
-                    printf("thirty");
-                    break;
-                }
-                case 4: {
-                    printf("forty");
-                    break;
-                }
-                case 5: {
-                    printf("fifty");
-                    break;
-                }
-                case 6: {
-                    printf("sixty");
-                    break;
-                }
-                case 7: {
-                    printf("seventy");
-                    break;
-                }
-                case 8: {
-                    printf("eighty");
-                    break;
-                }
-                case 9: {
-                    printf("ninety");
-                    break;
-                }
+        switch (num / 10) {
+            case 2: {
+                printf("twenty");
+                break;
             }
-            switch (num % 10) {
-                case 1: {
-                    printf("-one\n");
-                    break;
-                }
-                case 2: {
-                    printf("-two\n");
-                    break;
-                }
-                case 3: {
-                    printf("-three\n");
-                    break;
-                }
-                case 4: {
-                    printf("-four\n");
-                    break;
-                }
-                case 5: {
-                    printf("-five\n");
-                    break;
-                }
-                case 6: {
-                    printf("-six\n");
-                    break;
-                }
-                case 7: {
-                    printf("-seven\n");
-                    break;
-                }
-                case 8: {
-                    printf("-eight\n");
-                    break;
-                }
-                case 9: {
-                    printf("-nine\n");
-                    break;
-                }
+            case 3: {
+                printf("thirty");
+                break;
+            }
+            case 4: {
+                printf("forty");
+                break;
+            }
+            case 5: {
+                printf("fifty");
+                break;
+            }
+            case 6: {
+                printf("sixty");
+                break;
+            }
+            case 7: {
+                printf("seventy");
+                break;
+            }
+            case 8: {
+                printf("eighty");
+                break;
+            }
+            case 9: {
+                printf("ninety");
+                break;
+            }
+        }
+        switch (num % 10) {
+            case 1: {
+                printf("-one\n");
+                break;
+            }
+            case 2: {
+                printf("-two\n");
+                break;
+            }
+            case 3: {
+                printf("-three\n");
+                break;
+            }
+            case 4: {
+                printf("-four\n");
+                break;
+            }
+            case 5: {
+                printf("-five\n");
+                break;
+            }
+            case 6: {
+                printf("-six\n");
+                break;
+            }
+            case 7: {
+                printf("-seven\n");
+                break;
+            }
+            case 8: {
+                printf("-eight\n");
+                break;
+            }
+            case 9: {
+                printf("-nine\n");
+                break;
             }
         }
     }
